3797c4a95669a61f87ada23d8b9d282cb01be4ad
Noah018dev <140430451+Noah018dev@users.noreply.github.com>
2024-10-15 14:18:03 -0700

    Logviewer Added
    Better way to view the logs, also made it launch when the ai closes.

--- logviewer.py
+++ logviewer.py
@@ -1,10 +1,57 @@
+import contextlib
 from os import system
-from sys import argv
+from colorama import Fore, Back, Style
+
 
 system('title Nucleus - Utility [LOG-VIEWER]')
 system('cls')
 
+LogTypes = [
+    'DEBUG',
+    'INFO',
+    'WARN',
+    'ERROR',
+    'FATAL'
+]
 
+LogLevel = 2
+
+def PrintLogs():
+    with open('nucleus.log', 'r') as Log:
+        for LogLine in Log :
+            with contextlib.suppress(Exception):
+                for HigherLogs in range(5) :
+                    if LogLine.startswith(f'[{LogTypes[LogLevel + HigherLogs]}]') :
+                        match LogLevel + HigherLogs :
+                            case 0 :
+                                print(Style.DIM, end='')
+                            case 1 :
+                                print(Fore.LIGHTCYAN_EX, end='')
+                            case 2 :
+                                print(Fore.YELLOW, end='')
+                            case 3 :
+                                print(Fore.RED, end='')
+                            case 4 :
+                                print(Fore.MAGENTA, end='')
+                        print(LogLine, end=f'{Style.RESET_ALL}{Fore.RESET}{Back.RESET}')
+
+                    if LogTypes[LogLevel + HigherLogs] == 'FATAL' :
+                        break
+    
 
 while True :
-    ...
+    system('cls')
+    PrintLogs()
+    print(f'Log level {LogTypes[LogLevel]}.')
+
+    LogLevel = input('Enter log level [0 - 4] 0 = DEBUG, 4 = FATAL  >>>')
+
+    try :
+        LogLevel = int(LogLevel)
+    except Exception :
+        print('Invalid input.')
+        continue
+
+    if LogLevel < 0 or LogLevel > 4 :
+        print('Invalid input.')
+        continue
--- runtime-cleaner.cmd
+++ runtime-cleaner.cmd
@@ -1,8 +1,8 @@
 conhost --headless n-client.exe cmd /c python ai.py --finish-cleanup --silent
 n-client.exe cmd /c .\debugger.cmd
 %*
-start cmd /c start nucleus.log
 echo. > aidump.json
 timeout /t 2 /nobreak > nul
 conhost --headless n-client.exe cmd /c python ai.py --finish-cleanup --silent
+start n-client.exe python logviewer.py nucleus.log
 exit
