2844d8df12b7bd45947df9963fa60ead8f57f03f
Noah018dev <140430451+Noah018dev@users.noreply.github.com>
2024-10-16 20:23:03 -0700

    Small changes to make ai.py smaller
    Removed a few things and moves them to utils and datastore.
    Also made some modifications to make it backwards compatible, as before it would only work on python 3.13!
    Added charactersmgr.py, right now only has list of characters, but soon will have ability to load custom characters (when I add them).
    Changed .gitignore so Thumbs.db doesn't annoy me damnit.

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # Nucleus
- "AI with superpowers"
+## "AI with superpowers"
  Only the "Default system AI" has superpowers.
  Comes with 20 custom characters, but you also use the Custom (13) character to make your own. Type "!admin" in the custom character to create more advanced specifications, which was created to have you be able to make your own character and save them. DOESN'T WORK RIGHT NOW!
- Also btw "n-client.exe" is literally just a conhost.exe but I used Resource Hacker so I can get another logo 😎. 
+ 
+ Also btw "*n-client.exe*" is literally just a conhost.exe but I used Resource Hacker so I can get another logo 😎. 
 
  Warning : Doesn't work on POSIX or any other systems besides Windows. If you're on Linux or Mac, adding in scripts for your OS would be appreciated! :D
 
 # Launch
- Run "main.py"!!!
+Alright, now time to launch :
+
+1. In "API-KEY.txt", enter your OpenAI api key.
+
+2. Inside the project root directory, run "python *ai.py* --install requirements", and use "pip install -r *requirements.txt*".
+
+3. **Run *main.py***
 
 ¯\\_(ツ)_/¯
--- datastore.py
+++ datastore.py
@@ -1,15 +1,27 @@
 from os import chdir, path
+import datetime
 
 chdir(path.dirname(__file__))
 
-OpenAI_API_KEY = open('API-KEY.TXT').read() # REMOVE BEFORE RELEASED
+OpenAI_API_KEY = open('API-KEY.TXT').read()
 
 FileTypesInitalizedValue = {}
 
-HeaderSplit = '!---!' # WARNING : CHANGING CAN BREAK THE WHOLE SERVER
+HeaderSplit = '!-a--!' # WARNING : CHANGING CAN BREAK THE WHOLE SERVER
 
 BaseInjectLog = '//This code is used to log debugging information to console. "/inject_log.js"\nconsole.warn("Just another reminder to be careful with your API key!")\nconsole.log("Logging successfully injected into server file.")\n'
 
 email = 'noahc018@pm.me'
 
-SystemText = 'You are a helpful assistant designed to output JSON and be integrated with the system. Put your main text in the \"text\", and put any cmd text to execute in the \"cmd\". Using the windows cmd. Also have a \"quitting\" which is either 0 or 1, make it 1 if the user is trying to quit by entering something like \"quit\" or \"exit\".'
+def DefaultSystemAI(SelectedOutputForm, SearchTypes) -> dict[str, str] :
+    """
+    Returns a default system message for the AI.
+
+    Returns:
+        dict[str, str]: A dictionary with the role as 'system' and the content as the default system message.
+    """
+    date = datetime.now()
+    return {
+        'role' : 'system',
+        'content' : f'output json, have "content" be the text you want the user to see, and have "exit" be 1 if the user wants to exit the conversation, 0 if not. have "mini" "short" and "long" "extra_long" be shorter and longer versions of what you just said, you only need to generate the "{SelectedOutputForm}" and mini, though. make sure the json is valid json, not python dict. you will be an ai that can control the host\'s system, put a windows cmd command to execute in "psh". if you don\'t need "psh", just set it to an empty string, don\'t remove it. remember that if the user needs you to do something with their user folder, it will always be %userprofile%.. to generate images, set "img" to 1, and set "imgprompt" to what the prompt should be, give the image generator long descriptive prompts. if you don\'t need any of the outputs, like imgprompt, or psh, just set it to 0, or an empty string, whichever is appropriate. if you need to move the image anywhere using psh, the image generated from imgprompt will be placed in "C:\\Nucleus\\rimage.png", you can move and rename as you like. your name is Nucleus. if the user wants to exit, ask them if the are sure. if the user specificly wants you to generate and open an image, after generating, make your psh value be "openimg" you were made by Noah (me). If you want to search something, use "query", and it will be put in a new system message. You will be able to talk and respond again after you search. You can make as many searches as you need. Search when the user asks something about the latest update or the news, anything political, if they ask about the newest thing. Make all your searches of {date.strftime("%d of %B %Y")}. Please list your sources for your web search in the key "qsources" in the format of a python list, and feel free to make more searches just to make sure, for example, if you\'re searching for "Newest minecraft update", you can search more about it, just search at least more than once! You can search more than once by setting query to your first, then after you get those results search again a second and third time. Also search for info that needs to be up to date, like populations, weather, news, or anything like that. Make sure you\'re not using any fan made content when you\'re searching! If you don\'t know what something is, again, search with "query" if you don\'t know what it is. For example, the user says "tell me about oona from five worlds", and you don\'t know what it is, just search it! Here is the current time, in the users\'s timezone {date.strftime("%H:%M:%S")}. If you want to open a web page on the users computer, use weblnk, make sure the url is full, like "https://example.com/". Only answer the user\'s latest command or question. Make the key "stype" be of what you want the search\'s type to be. It can be any of these : {SearchTypes}. For searching, make sure to use 2 types, the first one is "search", and the second one is the type you want. If you use maps, also use places.'
+    }
